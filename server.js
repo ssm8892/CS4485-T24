458de9d40dc8f3b31b6c47858aa7609ba660c420
thomaskahng <thomaskahng1@gmail.com>
2023-04-26 01:17:47 -0500

    trying to fix backend

--- server.js
+++ server.js
@@ -56,138 +56,14 @@ app.all('*', (req, res) => {
   res.send("Invalid route");
 })
 
-//Show information about all tutors
-app.get('/tutors', (req, res) => {
-  pool.query('SELECT * FROM tutor', (error, results) => {
-    if (error) {
-      return res.status(500).json({ error });
-    }
-    res.json(results);
-    console.log(res);
-  });
-});
-
-// Show information about one specific tutor
-app.get('/tutors/:id', async (req, res) => {
-    try {
-        // Extract the tutor ID from the request parameters
-        const tutorId = req.params.id;
-        // Query the database for the tutor with the given ID
-        const [rows] = await pool.query('SELECT * FROM tutors WHERE id = ?', [tutorId]);
-        // Return the tutor information as JSON
-        res.json(rows[0]);
-    } catch (err) {
-        // Return an error response if something goes wrong
-        res.status(500).json({ error: 'An error occurred while retrieving the tutor' });
-    }
-});
-
-
-// Show "signup to be a tutor" page
-app.get('/tutors/new', (req, res) => {
-    // Return the HTML page for signing up to be a tutor
-    res.send('<html><body><h1>Sign up to be a tutor</h1></body></html>');
-});
-
-// Create new tutor account
-app.post('/tutors', async (req, res) => {
-    try {
-        // Extract the tutor information from the request body
-        const { name, subject } = req.body;
-
-        // Insert the new tutor into the database
-        const [result] = await pool.query('INSERT INTO tutors (name, subject) VALUES (?, ?)', [name, subject]);
-
-        // Return a JSON response indicating success
-        res.json({ message: 'Tutor account created successfully', tutorId: result.insertId });
-    } catch (err) {
-        // Return an error response if something goes wrong
-        res.status(500).json({ error: 'An error occurred while creating the tutor account' });
-    }
-});
-
 // Login as user
 app.post('/login', async (req, res, next) => {
   // Username and password
   const email_address = req.body.user_email;
   const password = req.body.user_password;
 
-  const [rows] = await pool.query('SELECT * FROM reservations WHERE email = ? and student_password = ?', 
-    [email_address], [password]);
-
-  const row_elems = rows.map(row => ({
-    email: row.email,
-    student_password: row.student_password
-  }))
-  res.json(row_elems)
-});
-
-// Lists all the reservations
-app.get('/reservations', async (req, res) => {
-    try {
-        // Retrieve list of reservations from the database
-        const [rows] = await pool.query('SELECT * FROM reservations');
-        const reservations = rows.map(row => ({
-            id: row.id,
-            tutor: row.tutor,
-            student: row.student,
-            datetime: row.datetime.toISOString()
-        }));
-
-        // Return the list of reservations as a JSON response
-        res.json(reservations);
-    } catch (err) {
-        // Return an error response if something goes wrong
-        res.status(500).json({ error: 'An error occurred while retrieving reservations' });
-    }
-});
-
-// Show information about one specific reservation
-app.get('/reservations/:id', async (req, res) => {
-    try {
-        // Retrieve the reservation with the given id from the database
-        const reservationId = req.params.id;
-        const [rows] = await pool.query('SELECT * FROM reservations WHERE id = ?', [reservationId]);
-        if (rows.length === 0) {
-            return res.status(404).json({ error: 'Reservation not found' });
-        }
-        const row = rows[0];
-        const reservation = {
-            id: row.id,
-            tutor: row.tutor,
-            student: row.student,
-            datetime: row.datetime.toISOString()
-        };
-
-        // Return the reservation information as a JSON response
-        res.json(reservation);
-    } catch (err) {
-        // Return an error response if something goes wrong
-        res.status(500).json({ error: 'An error occurred while retrieving the reservation' });
-    }
-});
-
-// Show new reservation form
-app.get('/reservations/new', (req, res) => {
-    // Return the HTML form for creating a new reservation
-    res.send('<html><body><h1>Create new reservation</h1></body></html>');
-});
-
-// Create new reservation
-app.post('/reservations', async (req, res) => {
-    try {
-        // Extract the reservation information from the request body
-        const { tutorId, studentName, datetime } = req.body;
-
-        // Insert the new reservation into the database
-        const [result] = await pool.query('INSERT INTO reservations (tutor_id, student_name, datetime) VALUES (?, ?, ?)', [tutorId, studentName, datetime]);
-
-        // Return a JSON response indicating success
-        res.json({ message: 'Reservation created successfully', reservationId: result.insertId });
-    } catch (err) {
-        // Return an error response if something goes wrong
-        res.status(500).json({ error: 'An error occurred while creating the reservation' });
-    }
+  console.log(email_address);
+  console.log(password);
 });
 
 app.listen(3000, () => {
