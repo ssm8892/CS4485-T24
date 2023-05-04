cd321a1e8b2da68be6e3cceea53832cf79f68493
ssm8892 <ssm48892@gmail.com>
2023-05-04 15:42:00 -0500

    dynamic calendar that loads real data

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
+
+//import { first } from "cheerio/lib/api/traversing";
+
 //
 // Scripts
 // 
@@ -52,12 +55,68 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 });
 
+function getDaysInMonth(month, year) {
+    console.log(month);
+    const tempDate = new Date(year, month, 0);
+    console.log(tempDate);
+    return tempDate.getDate();
+};
+
+
+function load_calendar() {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ['Janurary', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const calendar_div = document.getElementById("calendar_data_container");
+    const date = new Date();
+    console.log(new Date());
+    var first = new Date(date.getFullYear(), date.getMonth(), 1);
+    var firstDay = first.getDay();
+    console.log(firstDay);
+    var numDays = getDaysInMonth(date.getMonth(), date.getFullYear());
+    console.log(numDays);
+    var numWeeks = Math.ceil(numDays / 7);
+    console.log(numWeeks);
+    let j = 1;
+    const calendar_title = document.getElementById("calendar__picture");
+    let title_string1 = "<h2>" + date.getDate() + ", " + days[date.getDay()] + "</h2>";
+    let title_string2 = "<h3>" + months[date.getMonth()] + "</h3>";
+    calendar_title.innerHTML += title_string1;
+    calendar_title.innerHTML += title_string2;
+    while (numWeeks > 0) {
+        let i = 0;
+        const weekDiv = document.createElement("div");
+        weekDiv.classList.add("week_container", "text-white");
+        weekDiv.style.display = "flex";
+        weekDiv.style.flexDirection = "row";
+        while (i < 7) {
+            if (firstDay > 0) {
+                var string = "<div class='calendar__number'></div>";
+                weekDiv.innerHTML += string;
+                firstDay--;
+            } else if (numDays > 0) {
+                var string = "<div class='calendar__number'>" + j + "</div>";
+                weekDiv.innerHTML += string;
+                numDays--;
+                j++;
+            }
+            console.log(firstDay);
+            i++;
+        }
+        console.log(weekDiv.innerHTML);
+        numWeeks--;
+        calendar_div.appendChild(weekDiv);
+    }
+    console.log(calendar_div.innerHTML);
+}
+
+
+
 
 const tutorButtonForm = document.getElementById("form-label-tutor");
 const classButtonForm = document.getElementById("form-label-class");
 const tutorDivForm = document.getElementById("form-content-tutor");
 const classDivForm = document.getElementById("form-content-class");
-tutorButtonForm.onclick = function() {
+tutorButtonForm.onclick = function () {
     tutorDivForm.style.display = "flex";
     classDivForm.style.display = "none";
     classButtonForm.classList.remove("bg-white");
@@ -72,7 +131,7 @@ tutorButtonForm.onclick = function() {
     tutorButtonForm.classList.add("text-primary");
 };
 
-classButtonForm.onclick = function() {
+classButtonForm.onclick = function () {
     tutorDivForm.style.display = "none";
     classDivForm.style.display = "flex";
     tutorButtonForm.classList.remove("bg-white");
@@ -106,40 +165,37 @@ function displayFavs() {
 };
 
 var checkList = document.getElementById('list1');
-checkList.getElementsByClassName('anchor1')[0].onclick = function(evt) {
-  if (checkList.classList.contains('visible'))
-    checkList.classList.remove('visible');
-  else
-    checkList.classList.add('visible');
+checkList.getElementsByClassName('anchor1')[0].onclick = function (evt) {
+    if (checkList.classList.contains('visible'))
+        checkList.classList.remove('visible');
+    else
+        checkList.classList.add('visible');
 }
 
 var checkList2 = document.getElementById('list2');
-checkList2.getElementsByClassName('anchor2')[0].onclick = function(evt) {
-  if (checkList2.classList.contains('visible'))
-    checkList2.classList.remove('visible');
-  else
-    checkList2.classList.add('visible');
+checkList2.getElementsByClassName('anchor2')[0].onclick = function (evt) {
+    if (checkList2.classList.contains('visible'))
+        checkList2.classList.remove('visible');
+    else
+        checkList2.classList.add('visible');
 }
 
 
-function handleData()
-{
+function handleData() {
     var form_data = new FormData(document.querySelector("#tutor-signup-form"));
-    
-    if(!form_data.has("days[]"))
-    {
+
+    if (!form_data.has("days[]")) {
         document.getElementById("chk_option_error_days").style.visibility = "visible";
         return false;
-    }else if(!form_data.has("shifts[]")){
+    } else if (!form_data.has("shifts[]")) {
         document.getElementById("chk_option_error_shifts").style.visibility = "visible";
         return false;
     }
-    else
-    {
+    else {
         document.getElementById("chk_option_error_days").style.visibility = "hidden";
         document.getElementById("chk_option_error_shifts").style.visibility = "hidden";
-      return true;
+        return true;
     }
-    
+
 }
 
